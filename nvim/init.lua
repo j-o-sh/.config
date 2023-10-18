7a3f25418bacd34ca441d0f661594111dd6a090a
Joshua Hagedorn <joshua.hagedorn@sick.de>
2023-10-18 16:00:31 +0200

    modularize plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
+local plugins = require("plugins")
+
 -- Package Manager (lazy)
 function pm(plugins, options)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -15,88 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup(plugins, options)
 end
 
--- Plugins
-
-function plugins ()
-  return {
-    'tpope/vim-sleuth',
-    {
-      -- LSP Configuration & Plugins
-      'neovim/nvim-lspconfig',
-      dependencies = {
-        -- Automatically install LSPs to stdpath for neovim
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
-
-        -- Useful status updates for LSP
-        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-        { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
-        -- Additional lua configuration, makes nvim stuff amazing!
-        'folke/neodev.nvim',
-      },
-    },
-
-    {
-      -- Autocompletion
-      'hrsh7th/nvim-cmp',
-      dependencies = {
-        -- Snippet Engine & its associated nvim-cmp source
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-
-        -- Adds LSP completion capabilities
-        'hrsh7th/cmp-nvim-lsp',
-
-        -- Adds a number of user-friendly snippets
-        'rafamadriz/friendly-snippets',
-      },
-    },
-
-    {
-      -- Set lualine as statusline
-      'nvim-lualine/lualine.nvim',
-      -- See `:help lualine.txt`
-      opts = {
-        options = {
-          icons_enabled = false,
-          theme = 'onedark',
-          component_separators = '|',
-          section_separators = '',
-        },
-      },
-    },
-
-    { 'numToStr/Comment.nvim', opts = {} },
-
-    -- Fuzzy Finder (files, lsp, etc)
-    {
-      'nvim-telescope/telescope.nvim',
-      branch = '0.1.x',
-      dependencies = {
-        'nvim-lua/plenary.nvim',
-        {
-          'nvim-telescope/telescope-fzf-native.nvim',
-          build = 'make',
-          cond = function()
-            return vim.fn.executable 'make' == 1
-          end,
-        },
-      },
-    },
-
-    {
-      -- Highlight, edit, and navigate code
-      'nvim-treesitter/nvim-treesitter',
-      dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-      },
-      build = ':TSUpdate',
-    },
-  }
-end
-
-pm(plugins())
+pm(plugins)
 
 -- Window Options
 
