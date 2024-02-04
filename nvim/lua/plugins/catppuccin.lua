a155e70109340edbf2f7529f7092ebc47747f48d
Josh <59149060+j-o-sh@users.noreply.github.com>
2024-02-04 22:34:04 +0100

    reenable fast comments & theme transparency

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,5 +1,12 @@
 return { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {
   flavour = "mocha",
-  transparent_background = true
+  transparent_background = true,
+  highlight_overrides = {
+    all = function(colors)
+      return {
+        LineNr = { fg = colors.overlay1 }
+      }
+    end
+  }
 }}
 
