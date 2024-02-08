2b04074b3a6bb0706f924563550ad6611c784fc2
Joshua Hagedorn <joshua.hagedorn@sick.de>
2024-02-08 13:24:47 +0100

    simple completion

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,5 +1,4 @@
 local cmp = require('cmp')
-
 return {
   'hrsh7th/nvim-cmp',
   dependencies = {
@@ -12,6 +11,50 @@ return {
       expand = function()
       end,
     },
+
+    -- completion = {
+    --   completeopt = 'menu,menuone,noinsert',
+    -- },
+
+    mapping = cmp.mapping.preset.insert({
+
+      -- Safely select entries with <CR>
+      -- If nothing is selected (including preselections) add a newline as usual.
+      -- If something has explicitly been selected by the user, select it.
+      -- ['<CR>'] = {
+      --   i = function(fallback)
+      --     print('Wut?')
+      --     if cmp.visible() and cmp.get_active_entry() then
+      --       cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+      --     else
+      --       fallback()
+      --     end
+      --   end,
+      --   s = cmp.mapping.confirm({ select = true }),
+      --   c = cmp.mapping.confirm({
+      --     behavior = cmp.ConfirmBehavior.Replace,
+      --     select = true
+      --   }),
+      -- },
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        -- elseif luasnip.expand_or_locally_jumpable() then
+        --   luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        -- elseif luasnip.locally_jumpable(-1) then
+        --   luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+    }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp'},
       { name = 'buffer' },
