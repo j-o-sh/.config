3c308671249b2c886659919720496a927d5f33c2
Joshua Hagedorn <joshua.hagedorn@sick.de>
2024-06-25 16:58:19 +0200

    [fc] find changes in current branch

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,30 @@
+local function first_contained(datalist, items)
+  for _, i in ipairs(datalist) do
+    for _, j in ipairs(items) do
+      if i == j then return i end
+    end
+  end
+end
+
+local function browse_git_changes()
+  local pickers = require('telescope.pickers')
+  local finders = require('telescope.finders')
+  local conf = require('telescope.config').values
+
+  local current_branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
+  local branches = vim.fn.systemlist("git branch --format='%(refname:short)'")
+  local base = first_contained(branches, {'dev', 'main', 'master'})
+  local changed_files = vim.fn.systemlist("git diff --name-only " .. base .. " " .. current_branch)
+
+  pickers.new({}, {
+    prompt_title = 'Changed in current branch (to ' .. base .. ')',
+    finder = finders.new_table {
+      results = changed_files
+    },
+    sorter = conf.generic_sorter({})
+  }):find()
+end
+
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
@@ -13,6 +40,7 @@ return {
     wk.register({ ['<leader>fg'] = { builtin.live_grep, '[f]ind by [g]rep' } })
     wk.register({ ['<leader>fb'] = { builtin.buffers, '[f]ind in [b]uffers' } })
     wk.register({ ['<leader>fh'] = { builtin.help_tags, '[f]ind in [h]elp tags' } })
+
+    wk.register({ ['<leader>fc'] = { browse_git_changes, '[f]ind git [c]hanged files' } })
   end
 }
-
