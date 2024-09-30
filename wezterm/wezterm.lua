695cc59bb5d580b3d8084971f764dcf4f7cfa56d
Josh <59149060+j-o-sh@users.noreply.github.com>
2024-09-30 08:17:43 +0200

    wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,8 +4,17 @@ local config = wezterm.config_builder()
 config.color_scheme = 'catppuccin-mocha'
 config.font = wezterm.font 'Maple Mono NF'
 config.font_size = 13
+
+-- Tabs
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.9
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.tab_bar_at_bottom = true
+
+-- Cursor
+config.default_cursor_style = 'SteadyBar'
+config.cursor_thickness = 3
+
 config.window_decorations = 'RESIZE'
 config.window_padding = {
   left = '1cell',
@@ -13,5 +22,14 @@ config.window_padding = {
   top = '1.5cell',
   bottom = '0cell',
 }
+
+-- Background
+config.window_background_opacity = .8
+-- stange redraw bug where i see a ghost of the past tab, when switching to a new one?
+-- config.window_background_gradient = {
+--   colors = {'#1e1e2e88', '#11111bee' },
+--   -- Specifies a Linear gradient starting in the top left corner.
+--   orientation = { Linear = { angle = -45.0 } },
+-- }
 
 return config
